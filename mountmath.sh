d74097b378719530b0194ae584102e9b5c943be0
YourFin <d.junkpobox+github@gmail.com>
2017-11-20 23:08:24 -0600

    Fixed sshfs command

--- mountmath.sh
+++ mountmath.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 mkdir -p $SCRIPTPATH/mathlan-home-$1
-sshfs -o allow_other -C $1@ssh.cs.grinnell.edu:/home/$1 /mnt/mathlanSSH 
+sshfs -o allow_other -C $1@ssh.cs.grinnell.edu:/home/$1 $SCRIPTPATH/mathlan-home-$1
