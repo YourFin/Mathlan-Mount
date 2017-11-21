4c86f29bd245d27ad91f32e38d7a2db23c0a4cec
YourFin <d.junkpobox+github@gmail.com>
2017-11-20 22:15:59 -0600

    Fixed permission issue

--- mountmath.sh
+++ mountmath.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-sudo sshfs -o allow_other -C $1@ssh.cs.grinnell.edu:/home/$1 /mnt/mathlanSSH 
+SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
+mkdir -p $SCRIPTPATH/mathlan-home-$1
+sshfs -o allow_other -C $1@ssh.cs.grinnell.edu:/home/$1 /mnt/mathlanSSH 
 
